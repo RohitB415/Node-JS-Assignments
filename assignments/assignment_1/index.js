c12a565827594906f9aee5cb82b7f56f76c12e41
Rohit <rohitborse128@gmail.com>
2022-03-02 09:59:35 +0530

    My Node.js  Assignment-1 submission

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,17 +1,47 @@
-function getNameFromCommandLine() {
+const readline = require("readline");
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function getNameFromCommandLine(name) {
     // Write you code here, name should be taken as args in process.argv
+    return name
 }
 
 function getNameFromEnv() {
     // Write your code here
+    return process.env.name
 }
 
-function getNameFromReadLine() {
+function getNameFromReadLine(name) {
     // Write your code here
+    console.log(name);
 }
+
+
+
 
 module.exports = {
     getNameFromCommandLine,
     getNameFromEnv,
     getNameFromReadLine
 }
+
+
+process.env.name = "Rohit";
+
+getNameFromCommandLine(process.argv[2]);
+
+getNameFromEnv();
+
+
+
+
+r1.question("Please enter your name", (ans) => {
+    getNameFromReadLine(ans);
+    r1.close;
+})
+
+
+
